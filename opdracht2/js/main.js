0af5b0fac352e68bfe05ee7176587c0cd16429b3
Timo van den Berg <74822698+TimovandenBerg@users.noreply.github.com>
2023-01-01 21:22:52 +0100

    Zoeken werkt, maar werkt alleen nog me innerhtml

--- opdracht2/js/main.js
+++ opdracht2/js/main.js
@@ -45,3 +45,16 @@ function showCards(data) {
     }
     card.innerHTML = context;
 }
+function searchArtObject() {
+    var input = document.getElementById("js--searchbar").value;
+    input = input.toLowerCase();
+    var x = document.getElementsByClassName("cards__card");
+    for (let i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "block";
+        }
+    }
+}
--- opdracht2/js/main.ts
+++ opdracht2/js/main.ts
@@ -43,3 +43,18 @@ function showCards(data: {[x: string]: any; list: any; }) {
 
     card.innerHTML = context;
 }
+
+function searchArtObject() {
+    var input = document.getElementById("js--searchbar").value;
+    input = input.toLowerCase();
+    var x = document.getElementsByClassName("cards__card");    
+    
+    for (let i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "block";
+        }
+    }
+}
