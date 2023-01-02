34bc20cfd6121969979aee8de863ab90511d337b
Timo van den Berg <74822698+TimovandenBerg@users.noreply.github.com>
2023-01-02 23:37:45 +0100

    detail pagina af

--- opdracht2/js/detail-page.js
+++ opdracht2/js/detail-page.js
@@ -20,8 +20,37 @@ function getArtApi(url) {
 }
 getArtApi(art_url);
 function showDetail(data) {
-    console.log("Ik ben er");
+    var detailPagina = document.getElementById("detail");
     var context = '';
     var artObject = data.artObject;
     console.log(artObject);
+    // titel
+    var title = artObject.title;
+    // img
+    var img = artObject.webImage.url;
+    // beschrijving
+    var description = artObject.description;
+    // datum
+    var date = artObject.dating.presentingDate;
+    //maker
+    var artist = artObject.principalMaker;
+    // waar
+    var place = artObject.productionPlaces[0];
+    context +=
+        `<section class="detail__titel">
+            <h1>${title}</h1>
+        </section>
+        <section class="detail__img">
+            <picture><img src="${img}"></img></picture>
+            <p class="fs-500">Dit kunstwerk is gemaakt door: <spn class="red">${artist}</spn></p>        
+        </section>
+        <section>
+            <p class="fs-500">${description}</p>
+            <p class="fs-500">Het is gemaakt rond <spn class="red">${date}</spn> in <spn class="red">${place}</spn></p>
+        </section>
+        <section class="detail__terug">
+            <a href="index.html"><button class="fs-400">Terug</button></a>
+        </section>
+        `;
+    detailPagina.innerHTML = context;
 }
